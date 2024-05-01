f39fc443905e37ab86247457b16f37d3659e4c79
dharmendra763 <157779918+dharmendra-15@users.noreply.github.com>
2024-05-01 20:15:50 +0530

    first commit

---
+++ components/providers/RootProviders.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import { ThemeProvider } from "next-themes";
+
+function RootProviders({ children }: { children: ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
+
+export default RootProviders;
